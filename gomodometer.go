56356882b5c9693660872ef6ddec81b843dabd14
Gavin Cabbage <gavincabbage@gmail.com>
2016-01-24 15:42:12 -0500

    removed example, setup package files

---
+++ gomodometer.go
@@ -0,0 +1,24 @@
+package gomodometer
+
+import (
+	"fmt"
+)
+
+func TestFunction() {
+	fmt.Println("hello there")
+}
+
+// func main() {
+// 	f, err := os.Open("/dev/input/mice")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	readBuffer := make([]byte, 3)
+// 	for {
+// 		num, err := f.Read(readBuffer)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		fmt.Println("read", num, "bytes: ", readBuffer[0], readBuffer[1], readBuffer[2])
+// 	}
---
+++ gomodometer_test.go
@@ -0,0 +1,7 @@
+package gomodometer
+
+import "testing"
+
+func TestTestFunction(t *testing.T) {
+	TestFunction()
+}
