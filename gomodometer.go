61152e27860c5aef2eb4d2f081788d9395a5dc47
Gavin Cabbage <gavincabbage@gmail.com>
2016-01-24 21:40:10 -0500

    first draft of full package

--- gomodometer.go
+++ gomodometer.go
@@ -1,24 +1,75 @@
 package gomodometer
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
-func TestFunction() {
-	fmt.Println("hello there")
+const conversionCoefficient float64 = 0.000254
+
+type MouseOdometer struct {
+	deviceFile *os.File
+	readings   chan []byte
+	quit       chan struct{}
+	x, y       int
 }
 
-// func main() {
-// 	f, err := os.Open("/dev/input/mice")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	readBuffer := make([]byte, 3)
-// 	for {
-// 		num, err := f.Read(readBuffer)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		fmt.Println("read", num, "bytes: ", readBuffer[0], readBuffer[1], readBuffer[2])
-// 	}
+func NewMouseOdometer(deviceFileName string) *MouseOdometer {
+	f, err := os.Open(deviceFileName)
+	if err != nil {
+		return nil
+	}
+	return &MouseOdometer{deviceFile: f}
+}
+
+func (o *MouseOdometer) Start() {
+	o.setup()
+	go o.readMouseDevice()
+	go o.sumReadingsUntilQuit()
+}
+
+func (o *MouseOdometer) Stop() (float64, float64) {
+	if o.quit != nil {
+		o.quit <- struct{}{}
+	}
+	return convertToCentimeters(o.x), convertToCentimeters(o.y)
+}
+
+func (o *MouseOdometer) setup() {
+	o.x, o.y = 0.0, 0.0
+	o.readings = make(chan []byte)
+	o.quit = make(chan struct{})
+}
+
+func (o *MouseOdometer) readMouseDevice() {
+	buffer := make([]byte, 3)
+	for {
+		num, err := o.deviceFile.Read(buffer)
+		if err != nil || num != 3 {
+			panic(errors.New("problem reading file"))
+		}
+		o.readings <- buffer
+	}
+}
+
+func (o *MouseOdometer) sumReadingsUntilQuit() {
+	for {
+		select {
+		case <-o.quit:
+			return
+		case reading := <-o.readings:
+			o.x += normalizeReading(reading[1])
+			o.y += normalizeReading(reading[2])
+			fmt.Println(convertToCentimeters(o.x), " ", convertToCentimeters(o.y)) // gnuplot> plot 'mouse.dat' u 1:2 w l
+		}
+	}
+}
+
+func normalizeReading(raw byte) int {
+	return int(raw) - 128
+}
+
+func convertToCentimeters(raw int) float64 {
+	return float64(raw) * conversionCoefficient
+}
--- gomodometer_test.go
+++ gomodometer_test.go
@@ -1,7 +1,40 @@
 package gomodometer
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-func TestTestFunction(t *testing.T) {
-	TestFunction()
+const testFileName = "/tmp/gomodometer_test_file"
+
+func TestNewMouseOdometer(t *testing.T) {
+	createTestFile()
+	o := NewMouseOdometer(testFileName)
+	if o == nil || o.deviceFile == nil {
+		t.Fail()
+	}
+	os.Remove(testFileName)
+}
+
+func TestNormalizeReading(t *testing.T) {
+	var raw byte = 200
+	result := normalizeReading(raw)
+	if result != 72 {
+		t.Fail()
+	}
+}
+
+func TestConvertToCentimeters(t *testing.T) {
+	var raw int = 100
+	result := convertToCentimeters(raw)
+	if result > 0.03 || result < 0.02 { // should be 0.0254, epsilon = .01
+		t.Fail()
+	}
+}
+
+func createTestFile() {
+	os.Create(testFileName)
+	f, _ := os.Open(testFileName)
+	defer f.Close()
+	f.WriteString("111222333")
 }
